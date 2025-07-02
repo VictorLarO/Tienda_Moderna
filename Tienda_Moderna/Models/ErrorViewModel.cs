2073b0260561f702d32020302732a4c2ac5ed5e8
Victor Lara <167738630+VictorLarO@users.noreply.github.com>
2025-07-01 20:15:36 -0600

    Delete Tienda_Moderna directory

--- Tienda_Moderna/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Tienda_Moderna.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
