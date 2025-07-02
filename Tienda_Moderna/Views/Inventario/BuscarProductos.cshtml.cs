f1c1870a73994a21525c30dfba5f97ac37380423
Victor Lara <167738630+VictorLarO@users.noreply.github.com>
2025-07-01 20:28:30 -0600

    Add files via upload

---
+++ Tienda_Moderna/Views/Inventario/BuscarProductos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tienda_Moderna.Views.Inventario
+{
+    public class BuscarProductosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Tienda_Moderna/Views/Reportes/ReporteSemanal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tienda_Moderna.Views.Reportes
+{
+    public class ReporteSemanalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
